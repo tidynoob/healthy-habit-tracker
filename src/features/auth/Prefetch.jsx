135d555eadb0d711365b43c83fb63c5bb78f669a
tidynoob <mitchellgriffin9@gmail.com>
2023-02-04 02:01:58 -0500

    remove use of prefetching

--- src/features/auth/Prefetch.jsx
+++ src/features/auth/Prefetch.jsx
@@ -4,18 +4,22 @@ import { Outlet } from 'react-router-dom'
 // import { habitsApiSlice } from '../habits/habitsApiSlice'
 // import { usersApiSlice } from '../users/usersApiSlice'
 // import { pointsApiSlice } from '../points/pointsApiSlice'
+// import useAuth from '../../hooks/useAuth'
 
 function Prefetch() {
+  // const { id } = useAuth()
+  // console.log('id', id)
+
   useEffect(() => {
     // eslint-disable-next-line no-console
-    console.log('Prefetching data...')
-    // const habits = store.dispatch(habitsApiSlice.endpoints.getHabitsForUser.initiate())
+    // console.log('Prefetching data...')
+    // const habits = store.dispatch(habitsApiSlice.endpoints.getHabits.initiate())
     // const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
     // const points = store.dispatch(pointsApiSlice.endpoints.getPoints.initiate())
 
     return () => {
       // eslint-disable-next-line no-console
-      console.log('Unsubscribing from data...')
+      // console.log('Unsubscribing from data...')
       // habits.unsubscribe()
       // users.unsubscribe()
       // points.unsubscribe()
